89ac20e44446080459d79fbadb2e3e6d966d1f7e
Eric Bolinger <bolinger@wit.ad.cu.edu>
2016-12-27 18:51:15 -0700

    add WOL aliases

--- zsh_custom/alias.zsh
+++ zsh_custom/alias.zsh
@@ -1,2 +1,7 @@
 alias vpn='sudo openconnect --juniper --background --quiet --user=bolinger vpn.cusys.edu'
 alias psa='mosh dpsaadm01.dev.cu.edu -- tmux a -t eab0'
+
+# Wake On Lan
+alias wol_esb='sudo ether-wake b8:ae:ed:e9:8c:a4'
+alias wol_wit='sudo ether-wake 34:e6:d7:47:6c:c0'
+alias wol_porter='sudo ether-wake 00:1f:c6:9b:98:36'
