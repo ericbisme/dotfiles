e3ce5f2bdf7d15ca53279b9972f1f7175544716d
Eric Bolinger <eric.bolinger@cu.edu>
2016-12-27 18:42:04 -0700

    move rdp functionality to functions

--- zsh_custom/function.zsh
+++ zsh_custom/function.zsh
@@ -1,4 +1,5 @@
-# Function to set window Title
+#!/bin/zsh
+## Function to set window Title
 settitle() {
     printf "\033k$1\033\\"
 }
@@ -12,4 +13,27 @@ ssh() {
 
 ansible-sh() {
     ansible $1 -m shell -a $2
+}
+
+rdp() {
+    if [ ! -d ~/.dotfiles/remmina ]; then
+      echo ".remmina does not exist in your home directory"
+      return 1
+    fi
+
+    ping -c1 -W1 $1 > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+       echo "Server $1 is not reachable"
+       return 1
+    fi
+
+    #local CONFIG=`grep -l "server=$1" ~/.dotfiles/remmina/*.remmina`
+    local CONFIG=$(/usr/bin/grep -l "server=$1" $HOME/.remmina/*.remmina)
+    echo $CONFIG
+    if [ $? -ne 0 ]; then
+       echo "Server $1 is not available in your Remmina configurations"
+       return 1
+    fi
+
+    nohup remmina -c $CONFIG > /dev/null 2>&1 &
 }
