2dc02e3074dea519a6a3ef93859c5e3f3ad75461
Eric Bolinger <ericb@ericbisme.net>
2018-10-23 07:14:37 -0600

    aliases for kubectl and kubectl get all

--- zsh_custom/alias.zsh
+++ zsh_custom/alias.zsh
@@ -6,3 +6,7 @@ alias thepit='mosh thepit.homelinux.net --ssh="ssh -p 2222" -- tmux a -t eab0'
 alias wol_esb='sudo ether-wake b8:ae:ed:e9:8c:a4'
 alias wol_wit='sudo ether-wake 34:e6:d7:47:6c:c0'
 alias wol_porter='sudo ether-wake 00:1f:c6:9b:98:36'
+
+# Kubernetes
+alias kc='kubectl'
+alias kcga='kubectl get all'
