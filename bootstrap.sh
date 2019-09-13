4620bc7970d758ce3a8218a1f0cc209e3513b0ee
Eric Bolinger <ericb@ericbisme.net>
2019-09-13 15:56:37 -0600

    add a bootstrap script for initial setup

---
+++ bootstrap.sh
@@ -0,0 +1,26 @@
+# Make Required Directories
+mkdir -p ~/go/bin
+mkdir -p ~/.config/nvim/pack/minpac/opt
+
+#Oh-My-ZSH
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Powerline Go
+wget -O ~/go/bin/powerline-go https://github.com/justjanne/powerline-go/releases/download/v1.13.0/powerline-go-linux-amd64
+chmod +x ~/go/bin/powerline-go
+
+# Stern
+wget -O ~/go/bin/stern https://github.com/wercker/stern/releases/download/1.11.0/stern_linux_amd64
+chmod +x ~/go/bin/stern
+
+# neovim
+ln -s ~/.dotfiles/init.vim.symlink ~/.config/nvim/init.vim
+git clone https://github.com/k-takata/minpac.git ~/.config/nvim/pack/minpac/opt/minpac
+/usr/bin/nvim --headless -c PackUpdateAndQuit
+
+# Symlink Simple Configs
+ln -s ~/.dotfiles/tmux.conf.symlink ~/.tmux.conf
+ln -s ~/.dotfiles/zshrc.symlink ~/.zshrc.symlink
+#ln -s ~/.dotfiles/vimrc.after.symlink ~/.vimrc.after
+#ln -s ~/.dotfiles/vimrc.symlink ~/.vimrc
