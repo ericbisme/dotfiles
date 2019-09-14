c376892ad00dbb39eb046afac604343125e58617
Eric Bolinger <ericb@ericbisme.net>
2019-09-13 21:54:04 -0600

    update env script to match current reality

--- zsh_custom/env.zsh
+++ zsh_custom/env.zsh
@@ -1,9 +1,1 @@
-PATH="/home/bolinger/.dotfiles/bin:${PATH}" && export PATH;
-TNS_ADMIN="/tnsadmin"; export TNS_ADMIN;
-
-
-#PATH="/home/bolinger/perl5/bin${PATH:+:${PATH}}"; export PATH;
-#PERL5LIB="/home/bolinger/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"; export PERL5LIB;
-#PERL_LOCAL_LIB_ROOT="/home/bolinger/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"; export PERL_LOCAL_LIB_ROOT;
-#PERL_MB_OPT="--install_base \"/home/bolinger/perl5\""; export PERL_MB_OPT;
-#PERL_MM_OPT="INSTALL_BASE=/home/bolinger/perl5"; export PERL_MM_OPT;
+PATH="~/.dotfiles/bin:${PATH}" && export PATH;
